2941bf9031711e07ddc8c74f4551091e9437e1d3
alexalupescu <134335603+alexalupescu@users.noreply.github.com>
2024-02-27 22:48:41 +0200

    Update DirectIndex.java

--- DirectIndex.java
+++ DirectIndex.java
@@ -10,38 +10,38 @@ import java.util.LinkedList;
 import java.util.TreeMap;
 
 public class DirectIndex {
-    // preia textul din document si il pune intr-un fisier
+    // takes the text from the document and puts it in a file
     private static File getTextFromHTML(WebsiteInfo websiteInfo, Document doc, File html) throws IOException
     {
         StringBuilder sb = new StringBuilder();
-        sb.append(websiteInfo.getTitle(doc)); // titlul
+        sb.append(websiteInfo.getTitle(doc)); // title
         sb.append(System.lineSeparator());
-        sb.append(websiteInfo.getKeywords(doc)); // cuvintele cheie
+        sb.append(websiteInfo.getKeywords(doc)); // keywords
         sb.append(System.lineSeparator());
         sb.append(websiteInfo.getDescription(doc));
         sb.append(System.lineSeparator());
         sb.append(doc.body().text());
         String text = sb.toString();
 
-        // generam numere fisierului text corespunzator, cu extensia txt
+        // generate numbers for the corresponding text file, with the extension txt
         StringBuilder textFileNameBuilder = new StringBuilder(html.getAbsolutePath());
 
-        // adaugam extensia txt
+        // add the txt extension
         textFileNameBuilder.append(".txt");
         /*
-        // fisierele HTML ce contin "?" in nume vor primi extensia ".txt" alaturi de intregul nume
+        // HTML files containing "?" in the name they will receive the extension ".txt" along with the whole name
         if (textFileNameBuilder.indexOf("?") != -1)
         {
             textFileNameBuilder.append(".txt");
         }
-        else // daca nu, inlocuim extensia de dupa "." cu "txt"
+        else // if not, we replace the extension after "." with "txt"
         {
             textFileNameBuilder.replace(textFileNameBuilder.lastIndexOf(".") + 1, textFileNameBuilder.length(), "txt");
         }
         */
         String textFileName = textFileNameBuilder.toString();
 
-        // scriem rezultatul in fisierul text
+        // write the result in the text file
         FileWriter fw = new FileWriter(new File(textFileName), false);
         fw.write(text);
         fw.close();
@@ -49,65 +49,65 @@ public class DirectIndex {
         return new File(textFileName);
     }
 
-    // preia textul din fisier, caracter cu caracter si returneaza lista de cuvinte
+    // takes the text from the file, character by character and returns the list of words
     private static HashMap<String, Integer> processDocument(String fileName) throws IOException
     {
         HashMap<String, Integer> wordList = new HashMap<String, Integer>();
 
-        TreeMap<String, Double> tfList = new TreeMap<>(); // pentru stocarea tf-ului
-        int numberOfWordsInDocument = 0; // pentru calculul tf-ului
+        TreeMap<String, Double> tfList = new TreeMap<>(); // for storing the tf
+        int numberOfWordsInDocument = 0; // for calculating the tf
 
-        // citim din fisier caracter cu caracter
+        // we read from the file character by character
         FileReader inputStream = null;
         inputStream = new FileReader(fileName);
 
         // String delimiters = " \t\",.?!;:()[]{}@&#^%'`~<>/\\-–_|„”“=+*";
         StringBuilder sb = new StringBuilder();
 
-        int c; // caracterul curent
+        int c; // the current character
         while ((c = inputStream.read()) != -1)
         {
-            // if (delimiters.indexOf(textChars[i]) != -1) // suntem pe un separator
-            if (!Character.isLetterOrDigit((char)c)) // suntem pe un separator
+            // if (delimiters.indexOf(textChars[i]) != -1) // we are on a separator
+            if (!Character.isLetterOrDigit((char)c)) // we are on a separator
             {
-                String newWord = sb.toString(); // cream cuvantul nou
-                if (newWord.equals("")) // ignoram cuvantul vid
+                String newWord = sb.toString(); // create the new word
+                if (newWord.equals("")) // ignore the empty word
                 {
                     continue;
                 }
 
-                // mai intai, verificam daca este exceptie
+               // first, we check if it is an exception
                 if (ExceptionList.exceptions.contains(newWord))
                 {
-                    // il adaugam asa cum este
-                    if (wordList.containsKey(newWord)) // daca exista deja in HashMap
+                    // we add it as it is
+                    if (wordList.containsKey(newWord)) // if it already exists in the HashMap
                     {
-                        wordList.put(newWord, wordList.get(newWord) + 1); // incrementam numarul de aparitii
-                    } else // daca nu, il adaugam
+                        wordList.put(newWord, wordList.get(newWord) + 1); // we increase the number of occurrences
+                    } else // if not, we add it
                     {
                         wordList.put(newWord, 1);
                     }
                     ++numberOfWordsInDocument;
                 }
-                // apoi daca este stopword
+                // then if it is a stopword
                 else if (StopWordList.stopwords.contains(newWord))
                 {
-                    // il ignoram
+                    // we ignore it
                     sb.setLength(0);
                     continue;
                 }
-                else // cuvant de dictionar
+                else // dictionary word
                 {
-                    // se foloseste algoritmul Porter pentru stemming
+                    // the Porter algorithm is used for stemming
                     PorterStemmer stemmer = new PorterStemmer();
                     stemmer.add(newWord.toCharArray(), newWord.length());
                     stemmer.stem();
                     newWord = stemmer.toString();
 
-                    if (wordList.containsKey(newWord)) // daca exista deja in HashMap
+                    if (wordList.containsKey(newWord)) // if it already exists in the HashMap
                     {
-                        wordList.put(newWord, wordList.get(newWord) + 1); // incrementam numarul de aparitii
-                    } else // daca nu, il adaugam
+                        wordList.put(newWord, wordList.get(newWord) + 1); // we increase the number of occurrences
+                    } else // if not, we add it
                     {
                         wordList.put(newWord, 1);
                     }
@@ -115,21 +115,21 @@ public class DirectIndex {
                 }
 
                 // System.out.println(newWord + " -> " + hashText.get(newWord));
-                sb.setLength(0); // curatam StringBuilder-ul
+                sb.setLength(0); // we clean the StringBuilder
             }
-            else // suntem in mijlocul unui cuvant
+            else // we are in the middle of a word
             {
-                sb.append((char)c); // adaugam litera curenta la cuvantul ce se creeaza
+                sb.append((char)c); // add the current letter to the word being created
             }
         }
 
-        // eliminam cuvantul vid
+        // remove the empty word
         wordList.remove("");
 
-        // scriem in fisier lista de cuvinte si numarul de aparitii (index-ul direct)
+        // write in the file the list of words and the number of occurrences (the direct index)
         StringBuilder sbDirectIndexFileName = new StringBuilder(fileName);
 
-        // fisierul va avea extensia ".directindex.json" adaugata numelui original al fisierului HTML
+        // the file will have the extension ".directindex.json" added to the original HTML file name
         sbDirectIndexFileName.replace(sbDirectIndexFileName.lastIndexOf(".") + 1, sbDirectIndexFileName.length(), "directindex.json");
         Writer directIndexWriter = new BufferedWriter(new OutputStreamWriter(
                 new FileOutputStream(sbDirectIndexFileName.toString()), "utf-8"));
@@ -141,16 +141,16 @@ public class DirectIndex {
         directIndexWriter.close();
 
         inputStream.close();
-        // System.out.println("Cuvintele din textul de pe site au fost prelucrate!");
+        // System.out.println("The words in the text on the website have been processed!");
 
-        // pentru calculul tf
-        // cream HashMap-ul ce contine tf-ul pentru fiecare cuvant
+        // for tf calculation
+        // create the HashMap that contains the tf for each word
         for (String word : wordList.keySet())
         {
             tfList.put(word, (double)wordList.get(word) / numberOfWordsInDocument);
         }
 
-        // scriem intr-un fisier tf-ul pentru documentul curent
+        // write the tf for the current document in a file
         StringBuilder sbTfFileName = new StringBuilder(fileName);
         sbTfFileName.replace(sbTfFileName.lastIndexOf(".") + 1, sbTfFileName.length(), "tf.json");
         Writer tfWriter = new BufferedWriter(new OutputStreamWriter(
@@ -165,7 +165,7 @@ public class DirectIndex {
         return wordList;
     }
 
-    // calculeaza indexul direct
+    // calculate the index directly
     public static HashMap<String, HashMap<String, Integer>> directIndex(WebsiteInfo websiteInfo) throws IOException
     {
         HashMap<String, HashMap<String, Integer>> directIndex = new HashMap<>();
@@ -174,68 +174,68 @@ public class DirectIndex {
         String websiteFolder = websiteInfo.getWebsiteFolder();
         String baseUri = websiteInfo.getBaseUri();
 
-        // pentru obtinerea fisierului de mapare, numit "directindex.map", aflat in directorul radacina
+        // to get the mapping file, called "directindex.map", located in the root directory
         Writer mapFileWriter = new BufferedWriter(new OutputStreamWriter(
                 new FileOutputStream(websiteFolder + "directindex.map"), "utf-8"));
         HashMap<String, String> mapFile = new HashMap<>();
 
-        // pentru parcurgerea directoarelor, folosim o coada
+        // for directory traversal, we use a queue
         LinkedList<String> folderQueue = new LinkedList<>();
 
-        // pornim cu folder-ul radacina
+        // start with the root folder
         folderQueue.add(websiteFolder);
 
-        while (!folderQueue.isEmpty()) // cat timp nu mai sunt foldere copil de parcurs
+        while (!folderQueue.isEmpty()) // as long as there are no more child folders to go through
         {
             // preluam un folder din coada
             String currentFolder = folderQueue.pop();
             File folder = new File(currentFolder);
             File[] listOfFiles = folder.listFiles();
 
-            // ii parcurgem lista de fisiere / foldere
+            // we go through the list of files / folders
             try {
                 for (File file : listOfFiles)
                 {
-                    // daca am ajuns pe un fisier, verificam sa fie fisier de tip HTML
+                    // if we have arrived at a file, we check that it is an HTML file
                     if (file.isFile() && Files.probeContentType(file.toPath()).equals("text/html"))
                     {
-                        // parsam fisierul HTML folosind JSOUP
+                        // parse the HTML file using JSOUP
                         Document doc = Jsoup.parse(file, null, baseUri);
                         String fileName = file.getAbsolutePath();
-                        // System.out.println("[PARSARE] Am parsat fisierul HTML \"" + fileName + "\".");
+                        // System.out.println("[PARSARE] I parse the HTML file \"" + fileName + "\".");
 
                         // stocam textul in fisier separat
                         File textFile = getTextFromHTML(websiteInfo, doc, file);
                         String textFileName = textFile.getAbsolutePath();
-                        // System.out.println("[TEXT] Am preluat textul din fisierul HTML \"" + fileName + "\".");
+                        // System.out.println("[TEXT] I took the text from the HTML file \"" + fileName + "\".");
 
-                        // procesam cuvintele, rezultand un HashMap de tip (cuvant -> numar_aparitii)
-                        // stocam index-ul direct intr-un fisier cu extensia ".directindex"
+                        // process the words, resulting in a HashMap of type (word -> number_of_occurrences)
+                        // store the index directly in a file with the extension ".directindex"
                         HashMap<String, Integer> currentDocWords = processDocument(textFileName);
 
-                        // adaugam documentul si lista de cuvinte in HashMap-ul final
+                        // add the document and the list of words to the final HashMap
                         directIndex.put(fileName, currentDocWords);
 
-                        // adaugam documentul curent, impreuna cu index-ul direct asociat in fisierul de mapare
+                        // add the current document, together with the directly associated index in the mapping file
                         mapFile.put(fileName, fileName + ".directindex.json");
 
-                        // System.out.println("[INDEX_DIRECT] Am creat index-ul direct din fisierul TEXT \"" + textFileName + "\".");
+                        // System.out.println("[INDEX_DIRECT] I created the index directly from the TEXT file \"" + textFileName + "\".");
                     }
-                    else if (file.isDirectory()) // daca este folder, il punem in coada
+                    else if (file.isDirectory()) // if it's a folder, we put it in the queue
                     {
                         folderQueue.add(file.getAbsolutePath());
                     }
                 }
             } catch (NullPointerException e) {
-                // System.out.println("Nu exista fisiere in folderul \"" + currentFolder + "\"!");
+                // System.out.println("There are no files in the folder \"" + currentFolder + "\"!");
             }
         }
 
-        // scriem fisierul de mapare JSON
+        // write the JSON mapping file
         mapFileWriter.write(gsonBuilder.toJson(mapFile));
         mapFileWriter.close();
         // System.out.println(System.lineSeparator());
-        // System.out.println("[MAPARE] Am creat fisierul de mapare \"" + websiteFolder + "directindex.map\".");
+        // System.out.println("[MAPARE] I have created the mapping file \"" + websiteFolder + "directindex.map\".");
 
         return directIndex;
     }
